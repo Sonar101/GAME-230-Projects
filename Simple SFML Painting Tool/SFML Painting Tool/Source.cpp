c290ca94b9cc0241cd31649a0a9d596f24771d49
Nathann Latimore <nlatimor@ucsc.edu>
2022-09-27 21:11:53 -0700

    Added shape changing functionality

--- Simple SFML Painting Tool/SFML Painting Tool/Source.cpp
+++ Simple SFML Painting Tool/SFML Painting Tool/Source.cpp
@@ -13,8 +13,12 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
     
     // create shape
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::CircleShape circleBrush(100.f);
+    sf::RectangleShape squareBrush({100,100});
+    circleBrush.setFillColor(sf::Color::Green);
+    squareBrush.setFillColor(sf::Color::Green);
+
+    bool shapeIsCircle = true;
     
     // run while window is open
     while (window.isOpen())
@@ -25,33 +29,53 @@ int main()
             // checking single key press events for modifying brush settings
             if (event.type == sf::Event::KeyPressed)
             {
+                // --- changing brush shape settings
+                if (event.key.code == sf::Keyboard::S)
+                {
+                    if (shapeIsCircle)
+                    {
+                        std::cout << "Brush shape set to Square" << std::endl;
+                        shapeIsCircle = false;
+                    }
+                    else
+                    {
+                        std::cout << "Brush shape set to Circle" << std::endl;
+                        shapeIsCircle = true;
+                    }
+                }
+                
                 // --- changing brush color settings
                 if (event.key.code == sf::Keyboard::R)
                 {
                     std::cout << "Set color to red" << std::endl;
-                    shape.setFillColor(sf::Color::Red);
+                    circleBrush.setFillColor(sf::Color::Red);
+                    squareBrush.setFillColor(sf::Color::Red);
                 }
                 else if (event.key.code == sf::Keyboard::G)
                 {
                     std::cout << "Set color to green" << std::endl;
-                    shape.setFillColor(sf::Color::Green);
+                    circleBrush.setFillColor(sf::Color::Green);
+                    squareBrush.setFillColor(sf::Color::Green);
                 }
                 else if (event.key.code == sf::Keyboard::B)
                 {
                     std::cout << "Set color to blue" << std::endl;
-                    shape.setFillColor(sf::Color::Blue);
+                    circleBrush.setFillColor(sf::Color::Blue);
+                    squareBrush.setFillColor(sf::Color::Blue);
                 }
 
                 // --- changing brush size settings
-                if (event.key.code == sf::Keyboard::Up && shape.getRadius() <= 200.0f)
+                if (event.key.code == sf::Keyboard::Up && circleBrush.getRadius() <= 200.0f)
                 {
                     std::cout << "Increasing brush size by 5 pixels" << std::endl;
-                    shape.setRadius(shape.getRadius() + 5.0f);
+                    circleBrush.setRadius(circleBrush.getRadius() + 5.0f);
+                    
                 }
-                else if (event.key.code == sf::Keyboard::Down && shape.getRadius() >= 5.0f)
+                else if (event.key.code == sf::Keyboard::Down && circleBrush.getRadius() >= 5.0f)
                 {
                     std::cout << "Decreasing brush size by 5 pixels" << std::endl;
-                    shape.setRadius(shape.getRadius() - 5.0f);
+                    circleBrush.setRadius(circleBrush.getRadius() - 5.0f);
+                    squareBrush.setSize({squareBrush.getSize().x - 5.0f, squareBrush.getSize().y - 5.0f });
                 }
             }
             
@@ -61,12 +85,20 @@ int main()
         }
         
         // position shape to where the mouse is
-        shape.setPosition(sf::Mouse::getPosition(window).x - shape.getRadius(), sf::Mouse::getPosition(window).y - shape.getRadius());
+        circleBrush.setPosition(sf::Mouse::getPosition(window).x - circleBrush.getRadius(), sf::Mouse::getPosition(window).y - circleBrush.getRadius());
+        squareBrush.setPosition(sf::Mouse::getPosition(window).x - squareBrush.getSize().x / 2, sf::Mouse::getPosition(window).y - squareBrush.getSize().y / 2);
         
         // detect left mouse click to draw shape
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) 
         {
-            window.draw(shape);
+            if (shapeIsCircle)
+            {
+                window.draw(circleBrush);
+            }
+            else
+            {
+                window.draw(squareBrush);
+            }
         }
 
         window.display();
