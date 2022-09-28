5f98f03a3e9bc32b87bef194ff0e7b254705bb21
Nathann Latimore <nlatimor@ucsc.edu>
2022-09-27 22:30:35 -0700

    Added screenshot functionality, README file with controls, and fun screenshot

---
+++ Simple SFML Painting Tool/SFML Painting Tool/README.txt
@@ -0,0 +1,14 @@
+Controls
+--------------
+Color settings
+'r' Key - Changes brush color to red
+'g' Key - Changes brush color to green
+'b' Key - Changes brush color to blue
+
+Shape settings
+'c' Key - Changes brush to circle shape
+'s' Key - Changes brush to square shape
+
+Size settings
+Up Arrow - Increases brush size by 5 pixels
+Down Arrow - Decreases brush size by 5 pixels
--- Simple SFML Painting Tool/SFML Painting Tool/Source.cpp
+++ Simple SFML Painting Tool/SFML Painting Tool/Source.cpp
@@ -8,13 +8,41 @@
 #include <string>
 #include <iostream>
 
+/*
+Controls
+--------------
+Color settings
+'r' Key - Changes brush color to red
+'g' Key - Changes brush color to green
+'b' Key - Changes brush color to blue
+
+Shape settings
+'c' Key - Changes brush to circle shape
+'s' Key - Changes brush to square shape
+
+Size settings
+Up Arrow - Increases brush size by 5 pixels
+Down Arrow - Decreases brush size by 5 pixels
+*/
+
+
 int main()
 {
+    const float DEFAULT_BRUSH_SIZE = 50.0f;
+    const float MIN_BRUSH_SIZE = 5.0f;
+    const float MAX_BRUSH_SIZE = 500.0f;
+    
+    // initialize the render window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
     
-    // create shape
-    sf::CircleShape circleBrush(100.f);
-    sf::RectangleShape squareBrush({100,100});
+    // set up a texture and image to enable screenshot saving
+    sf::Texture texture;
+    texture.create(window.getSize().x, window.getSize().y);
+    sf::Image image;
+    
+    // create shape types for the brush
+    sf::CircleShape circleBrush(DEFAULT_BRUSH_SIZE);
+    sf::RectangleShape squareBrush({ DEFAULT_BRUSH_SIZE,DEFAULT_BRUSH_SIZE });
     circleBrush.setFillColor(sf::Color::Green);
     squareBrush.setFillColor(sf::Color::Green);
 
@@ -32,16 +60,13 @@ int main()
                 // --- changing brush shape settings
                 if (event.key.code == sf::Keyboard::S)
                 {
-                    if (shapeIsCircle)
-                    {
-                        std::cout << "Brush shape set to Square" << std::endl;
-                        shapeIsCircle = false;
-                    }
-                    else
-                    {
-                        std::cout << "Brush shape set to Circle" << std::endl;
-                        shapeIsCircle = true;
-                    }
+                    std::cout << "Brush shape set to Square" << std::endl;
+                    shapeIsCircle = false;
+                }
+                else if (event.key.code == sf::Keyboard::C)
+                {
+                    std::cout << "Brush shape set to Circle" << std::endl;
+                    shapeIsCircle = true;
                 }
                 
                 // --- changing brush color settings
@@ -65,17 +90,30 @@ int main()
                 }
 
                 // --- changing brush size settings
-                if (event.key.code == sf::Keyboard::Up && circleBrush.getRadius() <= 200.0f)
+                if (event.key.code == sf::Keyboard::Up && circleBrush.getRadius() <  MAX_BRUSH_SIZE)
                 {
-                    std::cout << "Increasing brush size by 5 pixels" << std::endl;
                     circleBrush.setRadius(circleBrush.getRadius() + 5.0f);
+                    squareBrush.setSize({ squareBrush.getSize().x + 5.0f, squareBrush.getSize().y + 5.0f });
+                    std::cout << "Increasing brush size: " << circleBrush.getRadius() << std::endl;
+                    
                     
                 }
-                else if (event.key.code == sf::Keyboard::Down && circleBrush.getRadius() >= 5.0f)
+                else if (event.key.code == sf::Keyboard::Down && circleBrush.getRadius() > MIN_BRUSH_SIZE)
                 {
-                    std::cout << "Decreasing brush size by 5 pixels" << std::endl;
                     circleBrush.setRadius(circleBrush.getRadius() - 5.0f);
-                    squareBrush.setSize({squareBrush.getSize().x - 5.0f, squareBrush.getSize().y - 5.0f });
+                    squareBrush.setSize({ squareBrush.getSize().x - 5.0f, squareBrush.getSize().y - 5.0f });
+                    std::cout << "Decreasing brush size: " << circleBrush.getRadius() << std::endl;
+                }
+
+                // --- take a screenshot
+                if (event.key.code == sf::Keyboard::U)
+                {
+                    std::cout << "Saved screenshot" << std::endl;
+                    // update the texture with the current window
+                    texture.update(window);
+                    // save contents of texture to an image and save it
+                    image = texture.copyToImage();
+                    image.saveToFile("PaintingToolScreenshot.png");
                 }
             }
             
@@ -84,13 +122,14 @@ int main()
                 window.close();
         }
         
-        // position shape to where the mouse is
+        // position shapes to where the mouse is
         circleBrush.setPosition(sf::Mouse::getPosition(window).x - circleBrush.getRadius(), sf::Mouse::getPosition(window).y - circleBrush.getRadius());
         squareBrush.setPosition(sf::Mouse::getPosition(window).x - squareBrush.getSize().x / 2, sf::Mouse::getPosition(window).y - squareBrush.getSize().y / 2);
         
         // detect left mouse click to draw shape
         if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) 
         {
+            // draw either a circle or square depending on player settings
             if (shapeIsCircle)
             {
                 window.draw(circleBrush);
